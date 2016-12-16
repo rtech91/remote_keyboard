aa48378f4e49ee8e61753572ee65d71f3e2d26c7
Костюк Антон < >
2016-12-16 19:44:48 +0200

    Added comment for [let KEY_TAP_TIMEOUT]

--- frontEndAPI.js
+++ frontEndAPI.js
@@ -1,5 +1,6 @@
 // This script uses jquery.js as dependency
 // Load this script after jquery.js 
+// Sets visual release timeout after key has been pushed
 let KEY_TAP_TIMEOUT = 300;
 function pushButtonOnce(button) {
     $(button).addClass('push');
